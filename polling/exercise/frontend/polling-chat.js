242e4f7f51e835ceedd6fc82bf92bd1454638b82
gilbert-kiana <gilbertintabo@gmail.com>
2022-02-09 09:52:13 +0300

    Polling with settimeout completed

--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -16,12 +16,37 @@ chat.addEventListener("submit", function (e) {
 
 async function postNewMsg(user, text) {
   // post to /poll a new message
-  // write code here
+  const data = {
+    user,
+    text,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  return await fetch("/poll", options);
+  //const json = await res.json();
 }
 
 async function getNewMsgs() {
   // poll the server
-  // write code here
+  let json;
+  try {
+    const res = await fetch("/poll");
+    json = await res.json();
+  } catch (e) {
+    //backoff code
+    console.error("polling error", e);
+  }
+
+  allChat = json.msg;
+  render();
+  setTimeout(getNewMsgs, INTERVAL);
 }
 
 function render() {
